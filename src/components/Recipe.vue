3da3313e44865f04a266652983664b3c6e70158c
tleifj <tleifj@traviss-mbp-2.attlocal.net>
2017-08-08 20:42:55 -0700

    fixed bug and added check to make sure it does not add duplicate items

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -45,7 +45,10 @@
 				this.$http.delete('data/recipes/' + this.$route.params.id + '/ingredients/' + index + '.json')
 			},
 			addMeal: function(recipe) {
+				// Get current meal plan
 				let currentMealPlan = this.mealPlans[Object.keys(this.mealPlans)[Object.keys(this.mealPlans).length - 1]];
+				console.log(currentMealPlan);
+				// Get current list
 				let currentList = this.lists[Object.keys(this.lists)[Object.keys(this.lists).length - 1]];
 				console.log(currentList.listItems);
 				console.log(recipe.ingredients);
@@ -54,35 +57,62 @@
 					let item = ingredients[ingredient];
 					// Check if list exists
 					if ( currentList.listItems ) {
+						console.log('has list');
 						// Get list items as an array
-						var listValues = Object.values(currentList.listItems)
-						var itemIndex;
-						function checkObjProp(obj, element) {
-							for (var item in obj) {
+						// var listValues = Object.values(currentList.listItems)
+						// var propIndex;
+						// function checkObjProp(obj, element) {
+						// 	for (var prop in obj) {
+						// 		// If it's a unique property
+						// 	    if (obj.hasOwnProperty(prop)) {
+						// 	        // Get value of property (list prop)
+						// 	        var val = obj[prop];
+						// 	        if (val.name === element.name) {
+						// 	        	propIndex = prop;
+						// 	        	return true;
+						// 	        }
+						// 	    } 
+						// 	}
+						// }
+
+
+						for (var prop in currentList.listItems) {
+							console.log('gettinglist item...');
 								// If it's a unique property
-							    if (obj.hasOwnProperty(item)) {
-							        // Get value of property (list item)
-							        var val = obj[item];
-							        if (val.name === element.name) {
-							        	itemIndex = item;
-							        	return true;
+							    if (currentList.listItems.hasOwnProperty(prop)) {
+							    	console.log('has on props');
+							    	console.log(prop);
+							        // Get value of property (list prop)
+							        var val = currentList.listItems[prop];
+							        console.log(val);
+							        console.log(item);
+							        if (val.name == item.name) {
+							        	console.log('in list already');
+							        	this.$http.put('data/lists/' + currentList['.key'] + '/listItems/' + prop + '/quantity.json', item.quantity + item.quantity);
+							        	break;
+							        } else {
+							        	console.log('not in list');
+							        	this.$http.post('data/lists/' + currentList['.key'] + '/listItems/.json', item)
+							        	break;
+
 							        }
 							    } 
-							}
-						}
+							} 
+
 						// Check if ingredient is in current list
-						if ( checkObjProp(currentList.listItems, ingredient) ) {
-							console.log('item already in there!');
-							// If it is already in list, add to the quantity
-							let item = listValues.find(element => element.name === ingredient.name)
-							// let itemIndex = listValues.findIndex(element => element.name === ingredient.name)
-							// item.quantity += ingredient.quantity;
-							this.$http.put('data/lists/' + currentList['.key'] + '/listItems/' + itemIndex + '/quantity.json', item.quantity + item.quantity)
-							console.log(itemIndex);
-						} else {
-							console.log(item);
-							this.$http.post('data/lists/' + currentList['.key'] + '/listItems/.json', item)
-						}
+						// if ( checkObjProp(currentList.listItems, ingredients[ingredient]) ) {
+						// 	console.log('item already in there!');
+						// 	console.log(propIndex);
+						// 	// If it is already in list, add to the quantity
+						// 	let item = listValues.find(element => element.name === ingredients[ingredient].name)
+						// 	// let propIndex = listValues.findIndex(element => element.name === ingredient.name)
+						// 	// item.quantity += ingredient.quantity;
+							
+						// 	console.log(item);
+						// } else {
+						// 	console.log(item);
+						// 	this.$http.post('data/lists/' + currentList['.key'] + '/listItems/.json', item)
+						// }
 							
 					} else {
 						// Set checked status to false
