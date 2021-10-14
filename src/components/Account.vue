580c96a44250cde86125e1afcb28856c73ee836c
Travis <tleifj@gmail.com>
2021-10-14 16:42:54 -0700

    Adds most used ingredients to account page.

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,7 +1,16 @@
 <template>
 	<div>
-		<h1>Account</h1>
-		<span @click="check()">check</span>
+		
+
+		<div class="col-sm-12">
+			<h2>Account</h2>
+			<h4>Most Used Ingredients</h4>	
+			<ul class="list">
+				<li class="list-item" v-for="(item, index) in ingredientsUsed">			
+					{{item.total}} {{item.name}}
+				</li>	
+			</ul>	
+		</div>
 	</div>
 </template>
 
@@ -23,11 +32,42 @@
 					}
 				}
 				return mealsArray;
+			},
+			ingredientsUsed() {
+				let ingredientsArray = [];
+				let ingredientsTally = [];
+				for ( let i = 0; i < this.lists.length; i++) {
+					for (let property in this.lists[i]) {
+						if (property === 'listItems') {
+							let listItems = this.lists[i]['listItems'];
+							for (let item in listItems) {
+								// console.log(listItems[item].name);
+								ingredientsArray.push(listItems[item].name);
+							}
+							
+						}
+					}
+				}
+				for (let i = 0; i < ingredientsArray.length; i++) {
+					
+					if (ingredientsTally.filter(e => e.name === ingredientsArray[i]).length > 0) {
+						ingredientsTally.find(e => e.name == ingredientsArray[i]).total += 1;
+					} else {
+						ingredientsTally.push({name: ingredientsArray[i], total: 1 });
+					}
+				}
+				ingredientsTally.sort(function(a, b) {
+					return b.total - a.total;
+				});
+				return ingredientsTally;
 			}
 		},
 		methods: {
 			check: function() {
-				console.log(this.mostUsedMeals)
+				this.ingredientsUsed.forEach(element => {
+					console.log(element);
+				});
+				console.log(this.ingredientsUsed)
 			}
 		},
 		firebase: {
